7fc94d86a61a745def63c5eee382306e7aa9c75a
Raissa <raissamalves@gmail.com>
2024-10-14 17:13:07 -0300

    integrando funções lambda com sqs

---
+++ lambda/orders/orderEmailsFunction.ts
@@ -0,0 +1,22 @@
+import { Context, SQSEvent } from "aws-lambda";
+import * as AWSXRay from "aws-xray-sdk";
+
+AWSXRay.captureAWS(require("aws-sdk"));
+
+export async function name(event: SQSEvent, context: Context): Promise<void> {
+    event.Records.forEach((record) => {
+        console.log(record);
+        const body = JSON.parse(record.body);
+        console.log(body);
+    });
+
+    return;
+}
+
+
+
+
+
+
+
+
